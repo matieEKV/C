b1289eeb6f5e3334ba92dc681c4f28bad7651c6c
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-02-24 21:10:25 +0000

    Thu, Feb 24, 2022, 9:10 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    // ask user for input
+    int height = get_int("What is your desired height?");
+
+    // 
+
 }
