ce7595741fc37a8195f8f0e3fb12f941ea1bb0c8
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-02-24 22:28:38 +0000

    Thu, Feb 24, 2022, 10:28 PM +00:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -14,7 +14,7 @@ int main(void)
 // loop line by line
     for (int line=0; line<height; line++) {
         // printf("#\n");
-        for (int index=0; index<line; index++){
+        for (int index=0; index<=line; index++){
             printf("#");
         }
         printf("\n");
