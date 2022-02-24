80307ebb3c435c34a853992a1d93975367100095
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-02-24 20:44:07 +0000

    Thu, Feb 24, 2022, 8:44 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
 
-string name = get_string ("What is your name?\n")
+string name = get_string("What is your name? ");
 int main(void)
 {
-    printf("hello, %s, name \n");
+    printf("hello, %s, name\n");
 }
