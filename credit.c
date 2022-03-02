bc5ed09924578b2bcc642fdeb5aa75300d0d8ba3
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-03-02 10:53:56 +0000

    Wed, Mar 2, 2022, 10:53 AM +00:00

--- credit.c
+++ credit.c
@@ -1,7 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+
+
+long get_long(void);
+int checksum (long cc_number);
+
 int main(void)
+{
+
+}
+
+long get_long(void);
+{
+    long cc_number = get_long ("What is your credit card number?\n");
+    return cc_number;
+}
+
+int checksum (long cc_number);
 {
     
 }
