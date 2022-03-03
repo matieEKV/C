d7043872f6752ff8ba6d44955dbb77883a6db00b
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-03-03 22:42:12 +0000

    Thu, Mar 3, 2022, 10:42 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -28,7 +28,9 @@ int checksum (int cc_number)
     while (cc_number > 0)
     {
         cc_number /= 10;
-        alternate_number_sum += number_digit_sum ((cc_number % 10)* 2);
+
+        alternate_number_sum += cc_number % 10;
+        += number_digit_sum ((cc_number % 10)* 2);
         cc_number /= 10;
     }
     return alternate_number_sum;
