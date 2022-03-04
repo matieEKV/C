5144a6e8f98a09228de34bde12b3b7443dbebc72
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-03-04 13:35:10 +0000

    Fri, Mar 4, 2022, 1:35 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 
 long get_number (void);
-int checksum (long cc_number);
+int checksum1 (long cc_number);
+int checksum2 (long cc_number);
 int number_digit_sum (int number);
 
 int main(void)
@@ -10,7 +11,7 @@ int main(void)
     //ask user for input
     long cc_number = get_number();
 
-    int sum = checksum(cc_number);
+    int sum = checksum1(cc_number);
     //if (sum%10==0)
     {
         printf ("VALID\n");
@@ -26,31 +27,36 @@ long get_number(void)
     return get_long ("What is your credit card number?\n");
 }
 
-int checksum(long cc_number)
+int checksum1(long cc_number)
 {
     // printf ("checking input: %i\n", cc_number);
     int alternate_number_sum = 0;
     int double_digit_sum = 0;
-    int other_number_sum = 0;
 
-    //while (cc_number > 0)
+    while (cc_number > 0)
     {
-       // cc_number /= 10;
+        cc_number /= 10;
 
         //alternate_number_sum += cc_number % 10;
-       // double_digit_sum += number_digit_sum ((cc_number % 10)* 2);
-        //cc_number /= 10;
+        double_digit_sum += number_digit_sum ((cc_number % 10)* 2);
+        cc_number /= 10;
     }
-    while (cc_number > 0)
+
+    //printf ("alternate nums: %i\n", alternate_number_sum);
+    printf ("double digits: %i\n", double_digit_sum);
+    return alternate_number_sum + double_digit_sum;
+}
+
+int checksum2 (long cc_number)
+{
+    int other_number_sum = 0;
+while (cc_number > 0)
     {
         other_number_sum += cc_number%10;
         cc_number/=10;
         cc_number/=10;
         printf ("other number sum: %i\n", other_number_sum);
     }
-    //printf ("alternate nums: %i\n", alternate_number_sum);
-    //printf ("double digits: %i\n", double_digit_sum);
-    return alternate_number_sum + double_digit_sum;
 }
 
 int number_digit_sum(int number)
