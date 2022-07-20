017127d79fbabe20a224dbbfa1727b41d0fc4a76
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-07-20 22:07:50 +0000

    Wed, Jul 20, 2022, 10:07 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string text)
+
 int main(void)
 {
     string text = get_string("Text: ");
     printf ("%s\n", text);
+}
+
+int count_letters(string text)
+{
+    
 }
