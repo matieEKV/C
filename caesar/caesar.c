cb074c2f762910036df4c78745a854ad698b9cad
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-07-22 16:24:56 +0000

    Fri, Jul 22, 2022, 4:24 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -10,4 +10,12 @@ int main(int argc, string argv[])
         return 1;
     }
     if (isdigit(argv[2]))
+    {
+        printf("good\n");
+    }
+    else
+    {
+        printf ("Usage: ./caesar key\n");
+        return 1;
+    }
 }
