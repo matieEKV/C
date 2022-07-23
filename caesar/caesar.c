25413cd8d5738f3ea7214315fe21965c746368df
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-07-23 11:47:34 +0000

    Sat, Jul 23, 2022, 11:47 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 bool only_digits(string s);
 
 int main(int argc, string argv[])
 {
+//check if the main takes in 2 arguments
     if (argc != 2)
     {
         printf ("Usage: ./caesar key\n");
@@ -17,9 +19,13 @@ int main(int argc, string argv[])
         // printf("arg: %s\n", argv[1]);
         // string input = argv[1];
 
+//check if the bool is true, that is if the key cointains only digits
         if (only_digits(argv[1]) == true)
         {
-            printf("great\n");
+// convert input string into an int
+            int key = atoi (argv[1]);
+            //printf("the key is %i\n", key);
+            
         }
 
     }
@@ -45,10 +51,10 @@ bool only_digits(string s)
       }
       if (length == counter && length > 0)
         {
-            printf ("wow, good!\n");
+            // printf ("wow, good!\n");
             return true;
         }
-        printf("uh oh 1\n");
+        // printf("uh oh 1\n");
         return false;
 }
 
