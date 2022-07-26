7f1a30d6de779257a12e8f3fef983d3eadd32f73
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-07-26 21:31:16 +0000

    Tue, Jul 26, 2022, 9:31 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -16,7 +16,15 @@ int main(int argc, string argv[])
     }
     else
     {
-        int key_check (string s);
+        string s1 = "abcdefghijklmnopqrstuvwxyz";
+    if (strcasecmp(argv[1], s1) == 0)
+        {
+            printf("they are equal!/n");
+        }
+    else
+    {
+        printf("they are not the same\n");
+    }
     }
 }
 
