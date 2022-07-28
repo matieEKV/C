c0b0a7204e47a679d028cb841954b5ec5583aa01
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-07-28 16:24:00 +0000

    Thu, Jul 28, 2022, 4:24 PM +00:00

---
+++ substitution/test.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <ctype.h>
+
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+    else
+    {
+        qsort(&argv[1], argc - 1, sizeof(char *));
+
+    }
