134412de113ee770041603a070486d99f5d14d97
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-08-26 12:33:15 +0000

    Fri, Aug 26, 2022, 12:33 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -2,6 +2,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 // Points assigned to each letter of the alphabet
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
@@ -15,8 +16,8 @@ int main(void)
     string word2 = get_string("Player 2: ");
 
     // Score both words
-    int score1 = compute_score(word1);
-    int score2 = compute_score(word2);
+    int score1 = compute_score(tolower(word1));
+    int score2 = compute_score(tolower(word2));
 
     if (score1 > score2)
     {
@@ -34,5 +35,5 @@ int main(void)
 
 int compute_score(string word)
 {
-    // TODO: Compute and return score for string
+
 }
