7ea7c6056ed177c852ca09e6764a4eb8d760c9f5
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-08-26 12:22:56 +0000

    Fri, Aug 26, 2022, 12:22 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -18,7 +18,18 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("PLayer 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
