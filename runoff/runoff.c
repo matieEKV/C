cecf9ba233da4b24de06b154ba5dfd13c0a51724
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-17 19:52:42 +0000

    Thu, Nov 17, 2022, 7:52 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -133,10 +133,6 @@ bool vote(int voter, int rank, string name)
         if (strcmp(candidates[i].name, name) == 0)
         {
             preferences[voter][rank] = i;
-
-            int[] ballot = preferences[voter];
-            ballot[rank] = i;
-
             return true;
         }
     }
