e07874b093fb543e8f6e18969fe0a36d39564c3e
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-18 14:18:21 +0000

    Fri, Nov 18, 2022, 2:18 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -202,12 +202,11 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    int number_votes = candidates[0].votes;
     for (int i = 1; i < candidate_count; i++)
     {
         if (!candidates[i].eliminated)
         {
-            if (candidates[i].votes == number_votes)
+            if (candidates[i].votes == min)
             {
                 min = candidates[i].votes;
                 return true;
