ee9773eea319b98128cbd7581edc09a9b0e45ceb
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-18 12:27:53 +0000

    Fri, Nov 18, 2022, 12:27 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -142,7 +142,23 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    for (int i = 0; i < voter_count; i++)
+    {
+        int candidate_index = preferences[i][0];
+        if (!candidates[candidate_index].eliminated)
+            {
+                candidates[candidate_index].votes++;
+                printf ("%s %i\n", candidates[candidate_index].name, candidates[candidate_index].votes);
+            }
+
+        else
+        {
+            int cadidate_index = preferences[i][1];
+            candidates[candidate_index].votes++;
+        }
+
+    }
+
     return;
 }
 
