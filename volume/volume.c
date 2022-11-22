25bbef58164e8499124d12d760aa017d56b754c4
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-22 17:59:38 +0000

    Tue, Nov 22, 2022, 5:59 PM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[44];
-    fread(&header, sizeof(uint8_t), 44, file)
+    fread(&header, sizeof(uint8_t), 44, input);
 
     // TODO: Read samples from input file and write updated data to output file
 
