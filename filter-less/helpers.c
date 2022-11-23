37aa6d38d3f75b4ff7b5d2bc338cdd38e4ace652
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-23 22:27:26 +0000

    Wed, Nov 23, 2022, 10:27 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -26,28 +26,28 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            int sepiaBlue = round((image[i][j].rgbtBlue * 0.131) + (image[i][j].rgbtGreen * 0.534) + (image[i][j].rgbtRed * 0.272));
-            int sepiaGreen = round((image[i][j].rgbtBlue * 0.168) + (image[i][j].rgbtGreen * 0.686) + (image[i][j].rgbtRed * 0.349));
-            int sepiaRed = round((image[i][j].rgbtBlue * 0.189) + (image[i][j].rgbtGreen * 0.769) + (image[i][j].rgbtRed * 0.393));
-            printf ("%i\n", sepiaBlue);
+            int sepiaBlue = (image[i][j].rgbtBlue * 0.131) + (image[i][j].rgbtGreen * 0.534) + (image[i][j].rgbtRed * 0.272);
+            int sepiaGreen = (image[i][j].rgbtBlue * 0.168) + (image[i][j].rgbtGreen * 0.686) + (image[i][j].rgbtRed * 0.349);
+            int sepiaRed = (image[i][j].rgbtBlue * 0.189) + (image[i][j].rgbtGreen * 0.769) + (image[i][j].rgbtRed * 0.393);
+            //printf ("%i\n", sepiaBlue);
 
             if (sepiaBlue + sepiaGreen + sepiaRed - 765 > 0)
             {
                 if (sepiaBlue - 255 > 0)
                 {
-                    double excessBlue = sepiaBlue - 255;
+                    int excessBlue = sepiaBlue - 255;
                     image[i][j].rgbtBlue = sepiaBlue - excessBlue;
                     //printf("Excess Blue :%i\n", excessBlue);
                 }
                 if (sepiaGreen - 255 > 0)
                 {
-                    double excessGreen = sepiaGreen - 255;
+                    int excessGreen = sepiaGreen - 255;
                     image[i][j].rgbtGreen = sepiaGreen - excessGreen;
                     // printf("Excess Green: %i\n", excessGreen);
                 }
                 if (sepiaRed - 255 > 0)
                 {
-                    double excessRed = sepiaRed - 255;
+                    int excessRed = sepiaRed - 255;
                     image[i][j].rgbtRed = sepiaRed - excessRed;
                     // printf("Excess Red: %i\n", excessRed);
                 }
