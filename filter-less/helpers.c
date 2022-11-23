8b77b8699e38b5565d936676641588441de2e6f2
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-23 13:13:44 +0000

    Wed, Nov 23, 2022, 1:13 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,11 +3,12 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    int gscale_average = 0;
     for (int i = 0; i < width; i++)
     {
         for (int j = 0; j < height; j++)
         {
-            
+            gscale_average = (RGBTRIPLE.rgbtBlue + )
         }
     }
     return;
