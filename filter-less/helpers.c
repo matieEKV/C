ea750814c79b9df136e6c22508fa70f224c7c9dd
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-23 14:08:53 +0000

    Wed, Nov 23, 2022, 2:08 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,9 +25,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             gscale_average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
-            image[i][j].rgbtBlue = gscale_average;
-            image[i][j].rgbtGreen = gscale_average;
-            image[i][j].rgbtRed = gscale_average;
+            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtRed = sepiaRed;
         }
     }
     return;
