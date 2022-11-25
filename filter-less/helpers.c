bb41446a8cc1c94f594307efb511d5afa70d1f70
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-25 21:35:25 +0000

    Fri, Nov 25, 2022, 9:35 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -65,10 +65,11 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             temp = first;
             first =
 
-            0     n-1
-            1      n-2
-            2       n-3
-            3       n-4
+            0       n-1
+            1       n-1-1
+            2       n-2-1
+            3       n-3-1
+            x       n-1-x
 
 
         }
