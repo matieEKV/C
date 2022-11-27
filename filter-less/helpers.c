e03ad00f4a0a112c20e22323a39fc79c962ef7b6
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-27 20:40:00 +0000

    Sun, Nov 27, 2022, 8:40 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -92,7 +92,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
         {
             for (int j = 0; j < width; j++)
             {
-                image[i][j] = blurredPixel(height, width, copy, i, j);
+                image[i][j] = blurredPixel(height, width, copy, j, i);
             }
         }
     return;
