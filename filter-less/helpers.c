89bd872c56e53bd83e53cc0733124e9afca197a1
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-28 22:23:01 +0000

    Mon, Nov 28, 2022, 10:23 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -108,7 +108,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             for (int j = 0; j < width; j++)
             {
                 printf("\nGoing to blur %d.%d of %dx%d", i, j, height, width);
-                // image[i][j] = blurredPixel(height, width, copy, j, i);
+                image[i][j] = blurredPixel(height, width, copy, j, i);
             }
         }
     return;
