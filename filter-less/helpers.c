817abd0ca45f70ebde459a7a47b92d871a4e736f
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-28 17:58:55 +0000

    Mon, Nov 28, 2022, 5:58 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -141,7 +141,7 @@ RGBTRIPLE blurredPixel(int height, int width, RGBTRIPLE copy[height][width], int
     //     endY = y+1;
     // }
 
-    // int offXvalues[] = {-1, 1, -1, 0, 0, 0, 1, 1, 1};
+    // int offXvalues[] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
     // int offYvalues[] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
 
     for (int i = startX; i < height; i++)
