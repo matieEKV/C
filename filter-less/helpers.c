75ddd5ac210504138e674cb49cb03e5048106bb8
matieEKV <42643598+matieEKV@users.noreply.github.com>
2022-11-28 15:44:34 +0000

    Mon, Nov 28, 2022, 3:44 PM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -149,14 +149,14 @@ RGBTRIPLE blurredPixel(int height, int width, RGBTRIPLE copy[height][width], int
             rgbtGreen += copy[i][j].rgbtGreen;
             rgbtRed += copy[i][j].rgbtRed;
 
-            printf("\nPixel: R %d, G %d, B %d", rgbtRed, rgbtGreen, rgbtBlue);
+            // printf("\nPixel: R %d, G %d, B %d", rgbtRed, rgbtGreen, rgbtBlue);
             counter += 1.0;
         }
     }
-    averageBlue = round(rgbtBlue / counter);
+    averageBlue = rgbtBlue / counter;
     averageGreen = rgbtGreen / counter;
     averageRed = rgbtRed / counter;
-    printf("Average: R %d, G %d, B %d", averageRed, averageGreen, averageBlue);
+    printf("\nAverage: R %d, G %d, B %d\n", averageRed, averageGreen, averageBlue);
 
     RGBTRIPLE newValues;
     newValues.rgbtBlue = averageBlue;
